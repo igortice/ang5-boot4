7f6f896af1bece309d004f15523596850a99dbe1
Igor Rocha <igortice@gmail.com>
2018-02-25 15:16:12 -0300

    add layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { ContainerComponent } from './layout/container/container.component';
+import { MainComponent } from './layout/main/main.component';
+import { FooterComponent } from './layout/footer/footer.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContainerComponent,
+    MainComponent,
+    FooterComponent
   ],
   imports:      [
     BrowserModule
