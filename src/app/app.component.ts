acd0319b7ad37c0cfff6df55a9fad3bb81630f63
Igor Rocha <igortice@gmail.com>
2018-03-04 13:04:33 -0300

    better

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,5 @@ import { Component } from '@angular/core';
   styleUrls:   [ './app.component.scss' ]
 })
 export class AppComponent {
-  viewLayout   = [ 'show', 'hidden' ];
-  currentIndex = 0;
-
-  showLayout(): void {
-    this.currentIndex === 0 ? this.currentIndex = 1 : this.currentIndex = 0;
-  }
+  example = 0;
 }
